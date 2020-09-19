17caec8598735514f9d87fdc486a37b73ce26f7f
schau00 <sreejitchau@outlook.com>
2020-09-19 20:55:33 +0530

    A7

--- Lockdown/A7/2.c
+++ Lockdown/A7/2.c
@@ -1,3 +1,210 @@
 /*
 Q : Use the above program to find 3 roots of the equation xtan(x)=c where c is a user-input constant. Use both bisection method and Newton-Raphson method.
 */
+
+# include <stdio.h>
+# include <math.h>
+/*
+Function Declarations
+*/
+float function(float *, float *);
+float dervFunction(float *);
+void bisectionMethod(float*);
+void regulaFalsiMethod(float*);
+void newtonRhapsonMethod(float*);
+void bisectionRecursion(float*, float*, float*,int*, int, float*, int*);
+void regFalsiRecursion(float*, float*, float*,int*, int, float*, int*);
+void newRapRecursion(float*, float*, float*, int*, int, float*, int*);
+
+void main(){
+    /*
+    Function f(x) = xtanx - c (c is user input)
+    Derivative f`(x) = x*pow(secx,2) + tanx
+    */
+
+   printf("Enter the value of c : ");
+   float c;
+   scanf("%f", &c);
+
+   printf("\nSolving by Bisection Method : \n");
+   bisectionMethod(&c);
+   printf("\nSolving by Regula Falsi Method : \n");
+   regulaFalsiMethod(&c);
+   printf("\nSolving by Newton Rhapson Method : \n");
+   newtonRhapsonMethod(&c);
+    return;
+
+}
+
+
+/*
+Calculation of value of function at x
+*/
+
+float function(float *x, float *c){
+    return ((*x * tan(*x)) -  *c);
+}
+
+/*
+Calculation of Derivative of function a x
+*/
+
+float dervFunction(float *x){
+    float secx = 1.00/ cos(*x);
+    return (((*x) * pow(secx, 2)) + (tan(*x)));
+}
+
+/*
+BISECTION METHOD
+*/
+void bisectionMethod(float *c){
+    int guess = 0;
+    float x1, x2, e;
+    int currentIteration = 0, it;
+    printf("Enter the desired level of accuracy : ");
+    scanf("%f", &e);
+    printf("Enter desired number of iterations : ");
+    scanf("%d", &it);
+    while(guess != 3){
+    
+    printf("Guess left limit : ");
+    scanf("%f", &x1);
+    printf("Guess right limit : ");
+    scanf("%f", &x2);
+    bisectionRecursion(&x1, &x2, &e, &it, currentIteration,c, &guess);
+    
+    }
+    return;
+}
+
+void bisectionRecursion(float* x1, float* x2, float* e,int* it, int currentIteration, float* c, int *guess){
+    float f1 = function(x1, c);
+    float f2 = function(x2, c);
+
+    if(f1*f2 > 0){
+        printf("No root exixts in the guessed limits .\n");
+        return;
+    }
+
+    float x = (*x1 + *x2)/2.00;
+    float currentError = abs((*x1 - *x2)/x);
+    if(currentError == *e){
+        printf("Root : %f\n", x);
+        *guess = *guess + 1;
+        return;
+    }
+    if(currentIteration >= *it){
+        printf("Root could not be found within %d iterations.\n", *it);
+        return;
+    }
+
+    float f = function(&x, c);
+    if(f*f1 > 0){
+        *x1 = x;
+    }
+    else{
+        *x2 = x;
+    }
+    return bisectionRecursion(x1, x2, e, it, currentIteration+1, c, guess);
+
+}
+
+/*
+REGULAR FALSI OR FALSE POSITION METHOD
+*/
+
+void regulaFalsiMethod(float *c){
+    float x1, x2, e;
+    int currentIteration = 0, it;
+    printf("Enter the desired level of accuracy : ");
+    scanf("%f", &e);
+    printf("Enter desired number of iterations : ");
+    scanf("%d", &it);
+    int guess = 0;
+    while(guess != 3){
+    printf("Guess left limit : ");
+    scanf("%f", &x1);
+    printf("Guess right limit : ");
+    scanf("%f", &x2);
+    regFalsiRecursion(&x1, &x2, &e, &it, currentIteration, c, &guess);
+    
+    }
+    return;
+}
+
+void regFalsiRecursion(float* x1, float* x2, float* e,int* it, int currentIteration, float*c, int * guess){
+    float f1 = function(x1, c);
+    float f2 = function(x2, c);
+    if(f1*f2 > 0){
+        printf("No root exixts in the guessed limits .\n");
+        return;
+    }
+
+    float x = (*x1) + ((f1) * ((*x1 - *x2)/(f2 - f1)));
+    float currentError = abs((*x1 - *x2)/(float)x);
+    if(currentError < *e){
+        printf("Root : %f\n", x);
+        *guess = *guess + 1;
+        return;
+    }
+    if(currentIteration >= *it){
+        printf("Root could not be found within %d iterations.\n", *it);
+        return;
+    }
+
+    float f = function(&x, c);
+    if(f*f1 > 0){
+        *x1 = x;
+    }
+    else{
+        *x2 = x;
+    }
+    return regFalsiRecursion(x1, x2, e, it, currentIteration + 1, c, guess);
+}
+
+/*
+NEWTON RHAPSON METHOD
+*/
+
+void newtonRhapsonMethod(float *c){
+    float x, e, s;
+    int it, currentIteration = 0;
+    printf("Enter desired number of iterations : ");
+    scanf("%d", &it);
+    printf("Enter desired accuracy : ");
+    scanf("%f", &e);
+    int guess = 0;
+    while(guess != 3){
+    
+    printf("Enter initial guess : ");
+    scanf("%f", &x);
+    s = 0.00001;
+    newRapRecursion(&x, &e, &s, &it, currentIteration, c, &guess);
+    }
+    return;
+}
+
+void newRapRecursion(float* x, float* e, float* s, int* it, int currentIteration, float *c, int *guess){
+    float f = function(x, c);
+    float f1 = dervFunction(x);
+
+    if(abs(f1) < *s){
+        printf("Slope is too small . No root can be determined for the guess.\n");
+        return;
+    }
+
+    float x1 = *x - (f/f1);
+    float currentError = (x1 - *x)/(*x);
+    if(currentError < *e){
+        printf("Root : %f\n", x1);
+        *guess = *guess + 1;
+        return;
+    }
+    if(currentIteration >= *it){
+        printf("Root could not be found within %d iterations.\n", *it);
+        return;
+    }
+    
+    *x = x1;
+    return newRapRecursion(x, e, s, it, currentIteration + 1, c, guess);
+}
---
+++ Lockdown/A7/5.c
@@ -0,0 +1,247 @@
+/*
+Q : Using the above program solve the following system of equations :
+i.  x + y + z = 6
+    x + y - z = 0
+    x - y + z = 2
+
+ii. x + y + z = 3
+    2x + 3y + z = 6
+    x - y - z = -3
+
+iii. 2x + 4y + 2z = 15
+     2x + y + 2z = -5
+     4x + y - 2z = 0
+
+*/
+
+
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+# define N 5
+void gaussEliminationPivot(float [N][N], int *);
+void gaussElimination(float [N][N], int *);
+void Jacobi(float [N][N], int *);
+
+void main(){
+    float A[N][N];
+    memset(A, 0, sizeof(A[0][0]) * N * N);
+    int n = 3;
+
+    // 1st eqn
+    A[0][0] = 1;
+    A[0][1] = 1;
+    A[0][2] = 1;
+    A[1][0] = 1;
+    A[1][1] = 1;
+    A[1][2] = -1;
+    A[2][0] = 1;
+    A[2][1] = -1;
+    A[2][2] = 1;
+    A[0][3] = 6;
+    A[1][3] = 0;
+    A[2][3] = 2;
+    printf("\nSolution of first equation : \n");
+    printf("\nSolution by Gauss Elimination Method : ");
+    gaussElimination(A, &n);
+    printf("\nSolution by Gauss Elimination Method using pivot : ");
+    gaussEliminationPivot(A, &n);
+    printf("\nSolution by Jacobi Method : ");
+    Jacobi(A, &n);
+
+    // 2nd eqn
+    A[0][0] = 1;
+    A[0][1] = 1;
+    A[0][2] = 1;
+    A[1][0] = 2;
+    A[1][1] = 3;
+    A[1][2] = 1;
+    A[2][0] = 1;
+    A[2][1] = -1;
+    A[2][2] = -1;
+    A[0][3] = 3;
+    A[1][3] = 6;
+    A[2][3] = -3;
+    printf("\n");
+    printf("\nSolution of second equation : \n");
+    printf("\nSolution by Gauss Elimination Method : ");
+    gaussElimination(A, &n);
+    printf("\nSolution by Gauss Elimination Method using pivot : ");
+    gaussEliminationPivot(A, &n);
+    printf("\nSolution by Jacobi Method : ");
+    Jacobi(A, &n);
+
+    // 3rd eqn
+    A[0][0] = 2;
+    A[0][1] = 4;
+    A[0][2] = 2;
+    A[1][0] = 2;
+    A[1][1] = 1;
+    A[1][2] = 2;
+    A[2][0] = 4;
+    A[2][1] = 1;
+    A[2][2] = -2;
+    A[0][3] = 15;
+    A[1][3] = -5;
+    A[2][3] = 0;
+    printf("\nSolution of third equation : \n");
+    printf("\nSolution by Gauss Elimination Method : ");
+    gaussElimination(A, &n);
+    printf("\nSolution by Gauss Elimination Method using pivot : ");
+    gaussEliminationPivot(A, &n);
+    printf("\nSolution by Jacobi Method : ");
+    Jacobi(A, &n);
+
+}
+
+
+void gaussEliminationPivot(float A[N][N], int *n){
+    
+    int i, j, k;
+    for(i = 0; i < *n; i++){
+        
+        for(j = 0; j < i; j ++){
+            float m = *(*(A + j) + i);
+            float s = *(*(A + i) + i);
+
+            for(k = 0; k <= *n; k ++){
+            //    *(*(A + i) + k) *= m;
+            //    *(*(A + j) + k) *= s;
+               *(*(A + j) + k) = (*(*(A + j) + k) * s) - (*(*(A + i) + k) * m);
+            }
+        }
+
+        
+
+        for(j = i+1; j < *n; j ++){
+            float m = *(*(A + j) + i);
+            float s = *(*(A + i) + i);
+
+            for(k = 0; k <= *n; k ++){
+            //    *(*(A + i) + k) *= m;
+            //    *(*(A + j) + k) *= s;
+               *(*(A + j) + k) = (*(*(A + j) + k) * s) - (*(*(A + i) + k) * m);
+            }
+        }
+    }
+    // for(i = 0; i < *n; i++){
+    //     for(j = 0; j <= *n; j++){
+    //         printf("%f ", *(*(A + i) + j));
+    //     }
+    //     printf("\n");
+    // }
+
+    printf("Values of the variables : ");
+    for(i = 0; i < *n; i++){
+        printf("%f ", (*(*(A + i) + *n)/ *(*(A + i) + i)));
+    }
+}
+
+void gaussElimination(float A[N][N], int *x){
+    int i, j, k, l;
+    int n = *x;
+
+    for(i = 0; i < n; i ++){
+        for(j = i + 1; j < n; j++){
+            float s = *(*(A + i) + i);
+            float m = *(*(A + j) + i);
+            for(k = i; k <= n; k ++){
+                *(*(A + j) + k) = (*(*(A + j) + k) * s) - (*(*(A + i) + k) * m) ;
+            }
+        }
+    }
+
+    // for(i = 0; i < n; i++){
+    //     for(j = 0; j <= n; j++){
+    //         printf("%f ", *(*(A + i) + j));
+    //     }
+    //     printf("\n");
+    // }
+    float var[n];
+    for(i = n-1; i >= 0; i--){
+        float S = *(*(A + i) + n);
+        for(j = i + 1; j < n; j++){
+            S -= (*(*(A + i) + j) * var[j]);
+        }
+        float c = S / *(*(A + i) + i);
+        var[i] = c;
+        
+    }
+
+    printf("Values of the variables : ");
+    for(i = 0; i < n; i++){
+        printf("%f ", var[i]);
+    }
+}
+
+void Jacobi(float A[N][N], int *x){
+    int n = *x;
+    float E = 0.000001;
+    int i, j, check = 0;
+    // checking row , column dominance
+    for(i = 0; i < n; i++){
+        float sum = 0;
+        for(j = 0; j < n; j ++){
+            if(i != j){
+                sum += (*(*(A + i) + j));
+            }
+        }
+        if(sum > *(*(A + i) + i)){
+            check = 1;
+        }
+    }
+
+    for(i = 0; i < n; i++){
+        float sum = 0;
+        for(j = 0; j < n; j ++){
+            if(i != j){
+                sum += (*(*(A + j) + i));
+            }
+        }
+        if(sum > *(*(A + i) + i)){
+            check = 1;
+        }
+    }
+
+    if(check == 1){
+        printf("Coefficient matrix is not diagonally dominant.\nThe jacobi method does not converge.\n");
+        return;
+    }
+
+    float var[n], tempvar[n];
+    memset(var, 0, n * sizeof(var[0]));
+    memset(tempvar, 0, n * sizeof(tempvar[0]));
+    check = 0;
+    int loop = 0;
+    while(check != 1 && loop < 500){
+        for(i = 0; i < n; i ++){
+            float S = *(*(A + i) + n);
+            for(j = 0; j < n; j++){
+                if(i != j){
+                    S = S - (*(*(A + i) + j) * var[j]);
+                }
+            }
+            tempvar[i] = S/ (*(*(A + i) + i));
+        }
+        loop ++;
+        if(loop > 5){
+        check = 1;
+        
+        for(i = 0; i < n; i++){
+            if(abs((tempvar[i] - var[i]/ tempvar[i])) > E){
+                check = 0;
+            }
+        }
+        }
+        for(i = 0; i < n; i++){
+            var[i] = tempvar[i];
+        }
+    }
+
+    printf("Values of the variables : ");
+    for(i = 0; i < n; i++){
+        printf("%f ", var[i]);
+    }
+    
+
+}
